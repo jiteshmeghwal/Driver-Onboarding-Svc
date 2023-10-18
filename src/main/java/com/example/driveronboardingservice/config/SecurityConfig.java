0ff089f970a586e7df1c60ee2d3d006c7cae5f8b
jiteshmeghwal <jiteshmeghwal2006@gmail.com>
2023-10-18 19:55:47 +0530

    disable CSRF to allow POST requests

--- src/main/java/com/example/driveronboardingservice/config/SecurityConfig.java
+++ src/main/java/com/example/driveronboardingservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +24,7 @@ public class SecurityConfig {
         return http.authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
